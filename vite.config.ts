173976f3ac174fdb6dd354b1fa9fd6722b4c5fa1
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-11-28 21:45:25 +0200

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     build: {
+        chunkSizeWarningLimit: 600,
         rollupOptions: {
             output: {
                 manualChunks: {
