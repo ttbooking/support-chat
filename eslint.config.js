5b316465c1bf97630b2f3583228d6877136320df
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-04-08 14:55:54 +0200

    Update dependencies + ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...pluginVue.configs["flat/recommended"],
+    eslintConfigPrettier,
+    {
+        languageOptions: {
+            parserOptions: {
+                parser: tsParser,
+                sourceType: "module",
+            },
+        },
+    },
+];
