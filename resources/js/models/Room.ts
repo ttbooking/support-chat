beaad519913f2cedbc7494a3c2ff2f4970df6e8c
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-11-07 11:51:10 +0200

    User status (relative to room)

--- resources/js/models/Room.ts
+++ resources/js/models/Room.ts
@@ -22,7 +22,7 @@ export default class Room extends Model implements BaseRoom {
     @Attr(null) declare typingUsers?: string[];
 
     @BelongsTo(() => User, "creatorId") declare creator: User;
-    @BelongsToMany(() => User, () => RoomUser, "roomId", "userId") declare users: User[];
+    @BelongsToMany(() => User, { as: "status", model: () => RoomUser }, "roomId", "userId") declare users: User[];
     @BelongsToMany(() => Tag, () => RoomTag, "roomId", "tagName") declare tags: Tag[];
     @HasManyBy(() => User, "typingUsers") declare usersTyping: User[];
     @HasMany(() => Message, "roomId") declare messages: Message[];
--- resources/js/models/RoomUser.ts
+++ resources/js/models/RoomUser.ts
@@ -1,11 +1,14 @@
 import { Model } from "pinia-orm";
 import { Attr } from "pinia-orm/decorators";
+import type { UserStatus } from "vue-advanced-chat";
 
-export default class RoomUser extends Model {
+export default class RoomUser extends Model implements UserStatus {
     static entity = "roomUser";
 
     static primaryKey = ["roomId", "userId"];
 
     @Attr() declare roomId: string;
     @Attr() declare userId: string;
+    @Attr() declare state: "online" | "offline";
+    @Attr() declare lastChanged: string;
 }
