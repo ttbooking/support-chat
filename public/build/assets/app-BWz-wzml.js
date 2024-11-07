1794a0a7b8d5df4218f5ec53bbc45d12416abea2
daniser <daniser@users.noreply.github.com>
2024-11-07 15:51:44 +0000

    Compile Assets

--- public/build/assets/app-BWz-wzml.js
+++
@@ -1,2 +0,0 @@
-import{d as a,c as o,_ as s,o as n,a as t,e as i,i as p,p as r,v as c}from"./vuetify-BrC7OoX2.js";import"./advanced-chat-B527sa3p.js";const m=a({__name:"App",setup(u){return(e,_)=>(n(),o(s,{"room-id":e.$env.roomId,height:960},null,8,["room-id"]))}});t(m).use(i).use(p).use(r).use(c).mount("#standalone-chat");
-//# sourceMappingURL=app-BWz-wzml.js.map
