815063c7b9cced06f06ae4cf4eb11cf9e733bb2d
daniser <5169543+daniser@users.noreply.github.com>
2025-04-03 16:27:44 +0000

    Compile Assets

---
+++ public/build/assets/app-uEbNrL2K.js
@@ -0,0 +1,2 @@
+import{d as r,S as s,_ as a,c as n,o as _,a as u,e as i,i as c,p as d,v as m}from"./vuetify-C0g7IoXS.js";import"./advanced-chat-Cmokl2X9.js";const f=r({__name:"App",setup(o,{expose:t}){t();const e={SupportChat:s};return Object.defineProperty(e,"__isScriptSetup",{enumerable:!1,value:!0}),e}});function l(o,t,e,p,v,S){return _(),n(p.SupportChat,{"room-id":o.$env.roomId,height:960},null,8,["room-id"])}const h=a(f,[["render",l],["__file","App.vue"]]);u(h).use(i).use(c).use(d).use(m).mount("#standalone-chat");
+//# sourceMappingURL=app-uEbNrL2K.js.map
