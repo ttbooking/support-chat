f2264760e8b0eaf95f30b02aa3ca893c16fd2883
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-11-19 14:15:14 +0200

    Bug fix

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Workbench\App\Models\User;
 
-Route::view('/', 'lobby', [
+Route::get('/', static fn () => view('lobby', [
     'users' => User::all()->random(min(User::count(), 100)),
-])->name('login');
+]))->name('login');
