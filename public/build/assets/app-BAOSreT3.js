6a6ff457bee5e3290705f6a56d16291559271671
daniser <5169543+daniser@users.noreply.github.com>
2025-12-02 16:44:12 +0000

    Compile assets

--- public/build/assets/app-BAOSreT3.js
+++
@@ -1,2 +0,0 @@
-import{d as r,S as s,_ as a,c as n,o as _,a as u,e as i,i as c,p as d,v as m}from"./vuetify-BE9GzZVS.js";import"./advanced-chat-CMld-EBr.js";const f=r({__name:"App",setup(o,{expose:t}){t();const e={SupportChat:s};return Object.defineProperty(e,"__isScriptSetup",{enumerable:!1,value:!0}),e}});function l(o,t,e,p,v,S){return _(),n(p.SupportChat,{"room-id":o.$env.roomId,height:960},null,8,["room-id"])}const h=a(f,[["render",l],["__file","App.vue"]]);u(h).use(i).use(c).use(d).use(m).mount("#standalone-chat");
-//# sourceMappingURL=app-BAOSreT3.js.map
