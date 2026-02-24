ee92bf904fcd7b5cb5d3e4e193c2f4cfe99c415b
daniser <5169543+daniser@users.noreply.github.com>
2026-02-24 10:54:12 +0000

    Compile assets

--- public/build/assets/app-DoKNKWdR.js
+++
@@ -1,2 +0,0 @@
-import{d as r,S as s,_ as a,o as n,c as _,a as u,e as i,i as c,p as d,v as m}from"./vuetify-7_-0rJTx.js";import"./advanced-chat-CT6x4KUD.js";const f=r({__name:"App",setup(o,{expose:t}){t();const e={SupportChat:s};return Object.defineProperty(e,"__isScriptSetup",{enumerable:!1,value:!0}),e}});function l(o,t,e,p,v,S){return n(),_(p.SupportChat,{"room-id":o.$env.roomId,height:960},null,8,["room-id"])}const h=a(f,[["render",l],["__file","App.vue"]]);u(h).use(i).use(c).use(d).use(m).mount("#standalone-chat");
-//# sourceMappingURL=app-DoKNKWdR.js.map
